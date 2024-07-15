734c488c76ed15d3c49df564eeb06db2e8f81f5f
Lyle Keeton <lyle@effectivesolutions.dev>
2024-07-15 01:55:40 -0500

    websocket fix

--- websocket.go
+++ websocket.go
@@ -1,22 +1,36 @@
 package dotnet
 
 import (
+    "net"
     "net/http"
     "net/http/httputil"
     "net/url"
-
+    "go.uber.org/zap"
+    "context"
 )
 
 func (d DotNet) handleWebSocket(w http.ResponseWriter, r *http.Request) error {
-    proxy := httputil.NewSingleHostReverseProxy(&url.URL{Scheme: "http", Host: "unix", Path: d.Socket})
+    socketPath := d.generatedSocket // Use the generated socket path
+    
+    proxy := httputil.NewSingleHostReverseProxy(&url.URL{Scheme: "http", Host: "localhost"})
+    
+    proxy.Transport = &http.Transport{
+        DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+            return net.Dial("unix", socketPath)
+        },
+    }
     
     proxy.Director = func(req *http.Request) {
         req.URL.Scheme = "http"
-        req.URL.Host = "unix"
-        req.URL.Path = r.URL.Path
+        req.URL.Host = "localhost"
         req.Host = r.Host
     }
     
+    d.logger.Info("Proxying WebSocket request",
+        zap.String("path", r.URL.Path),
+        zap.String("socket", socketPath))
+    
     proxy.ServeHTTP(w, r)
+    
     return nil
 }
